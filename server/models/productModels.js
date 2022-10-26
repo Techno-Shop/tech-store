0ff306aed18ea9709145539784caacd41b5334ec
khalilbenali1 <khalil.ben.ali16121@gmail.com>
2022-10-26 14:34:24 +0100

    added front

--- server/models/productModels.js
+++ server/models/productModels.js
@@ -1,32 +1,30 @@
-const mongoose = require('mongoose');
-const productSchema= new mongoose.Schema({
-    name: {
-        type: String,
-        },
-    field: {
-        type: string,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    instock: {
-        type: Boolean,
-        default: true
-    },
-    description: {
-        type: String,
-    default:"latest available of this generation "
+const mongoose = require("mongoose");
 
-    },
-    image: {
-        type: Array,
-        required: true
-    }
-   
-
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  field: {
+    type: string,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  instock: {
+    type: Boolean,
+    default: true,
+  },
+  description: {
+    type: String,
+    default: "latest available of this generation ",
+  },
+  image: {
+    type: Array,
+    required: true,
+  },
 });
-const product= mongoose.model('tech', productSchema);
+const product = mongoose.model("tech", productSchema);
 
 module.exports = product;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3001;
@@ -8,22 +8,7 @@ const port = 3001;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/../client/dist"));
 
-
-mongoose
-  .connect("mongodb://localhost:27017/tech-store", { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    })
-  .then(() => console.log("Connected to mongoose server"))
-  .catch((err) => {
-    console.log(err)});
-
-
-    app.listen(port, () =>
-console.log(`app listening at http://localhost:${port}`)
+app.listen(port, () =>
+  console.log(`app listening at http://localhost:${port}`)
 );
-
-
-
