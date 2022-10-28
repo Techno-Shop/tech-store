f9095a4123ec239199b88b7b75022ba9f431c5ac
khalilbenali1 <khalil.ben.ali16121@gmail.com>
2022-10-28 16:07:47 +0100

    added header , completed admin interface

--- client/src/Admin/main/Main.jsx
+++ client/src/Admin/main/Main.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import AddProduct from './Comps/AddProduct'
-import DataTable from './Comps/DataTable'
-import "./MainSec.css"
+import React from "react";
+import AddProduct from "./Comps/AddProduct";
+import DataTable from "./Comps/DataTable";
+import "./MainSec.css";
 
 function Main(props) {
   return (
-    <div className='mainSec'>
-        <div className='loginSect'>
-            <p>test</p>
-        </div>
-        <div className='data'>
-        <DataTable cols={props.cols} rows={props.rows}/>
+    <div className="mainSec">
+      <div className="loginSect">
+        <p>Logged In Person</p>
+      </div>
+        <div className="data">
+          {props.view === "users" ? (
+            <DataTable cols={props.cols} rows={props.rows} check={false} />
+          ) : (
+            <DataTable cols={props.cols} rows={props.rows} check={true} />
+          )}
         </div>
     </div>
-  )
+  );
 }
 
 export default Main;
