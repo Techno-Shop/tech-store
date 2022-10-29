1055016672f5e41f291178a69bb7ebf6804ed2bb
aymensouissi <aymensouissi@gmail.com>
2022-10-29 18:17:02 +0100

    fixing merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Sidebar from "./Admin/Sidebar/Sidebar";
 
 
 import Carousele from "./component/Carousele";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -23,24 +23,27 @@ import axios from "axios";
 
 
 function App() {
-const [data, setData] = useState([]);
-    const user = { isConnected: false }
+  const [data, setData] = useState([]);
+  const user = { isConnected: false }
   useEffect(() => {
     axios.get("http://localhost:3001/product/all").then((res) => {
       setData(res.data);
     })
   }, []);
-  
+
   return (
     <div className="App">
       <Router>
-        <Routes>   
- <Route exact path="/"  index element={< NavbarComp />} />
- <Route  path="/carousel"   element={< Carousele />} />
+        <Routes>
+
+
+
+
+
           <Route exact path="/" index element={< NavbarComp />} />
-          <Route  path="/login" element= {<Login/>} />
-          <Route  path="/Register" element= {<Register/>} />
-          <Route  path="/logout" element= {<LogOut/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/logout" element={<LogOut />} />
           <Route path="/admin" element={<Admin />} />
         </Routes>
       </Router>
